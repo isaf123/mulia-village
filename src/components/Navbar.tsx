c8455a077d4e383cd87a10beb5ae6c25ef03ea8f
isaf123 <f.isa283@gmail.com>
2024-03-27 07:40:35 +0700

    updated navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import Image from "next/image";
+
+interface INavbarDesktopProps {}
+
+const NavbarDesktop: React.FunctionComponent<INavbarDesktopProps> = (props) => {
+  return (
+    <div className="w-full h-fit flex justify-between bg-white px-10 py-8">
+      <Image src={"/logo-muliavillage.png"} alt="" width={20} height={16} />
+      <div
+        className="text-lg text-color1 flex gap-6
+      "
+      >
+        <p>Home</p>
+        <p>About Us</p>
+        <p>Contact</p>
+        <p>Product</p>
+        <p>Site plan</p>
+        <p>Info</p>
+        <p>Promo</p>
+      </div>
+    </div>
+  );
+};
+
+export default NavbarDesktop;
