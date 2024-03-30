b7ce0536742ed9421a89c9e6f4681b7abeb18665
VRay92 <adrian.irawan92@gmail.com>
2024-03-30 20:34:35 +0700

    fix card hover

--- src/components/Card1.tsx
+++ src/components/Card1.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState } from "react";
 
 interface ICard1Props {
   children: any;
@@ -8,12 +9,29 @@ interface ICard1Props {
 }
 
 const Card1: React.FunctionComponent<ICard1Props> = (props) => {
+  const [onHover, setOnHover] = useState<boolean>(false);
   return (
-    <div className="h-[504px] w-1/3 rounded-xl shadow-lg flex flex-col text-center text-3xl">
-      <div className="flex justify-center my-10 ">{props.children}</div>
-      <h1 className="font-bold mx-10">{props.title}</h1>
-      <h1 className="mt-[6rem] mx-10">{props.number}</h1>
-      <h1>{props.date}</h1>
+    <div
+      className={`relative transition duration-500 h-[504px] w-1/3 rounded-xl shadow-lg flex flex-col text-center text-3xl ${
+        onHover ? "bg-color1 text-white" : ""
+      }`}
+      onMouseEnter={() => setOnHover(true)}
+      onMouseLeave={() => setOnHover(false)}
+    >
+      <div className="flex justify-center">
+        <span
+          className={`flex justify-center my-10 rounded-full border-2 ${
+            onHover ? "border-color2 text-color2" : "border-black text-black"
+          } h-[4rem] w-[4rem] items-center`}
+        >
+          {props.children}
+        </span>
+      </div>
+      <h1 className="font-bold mx-8">{props.title}</h1>
+      <div className="absolute bottom-16 text-center w-full">
+        <h1 className="mx-auto">{props.number}</h1>
+        <h1 className="mx-auto">{props.date}</h1>
+      </div>
     </div>
   );
 };
