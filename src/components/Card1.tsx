4ff2d36af7ee8db9f544d225cd5ffc5bc1dfe462
Adrian Irawan <150063940+VRay92@users.noreply.github.com>
2024-04-02 01:49:24 +0700

    fixed responsive section lokasi strategis, legalitas terjamin (#7)

--- src/components/Card1.tsx
+++ src/components/Card1.tsx
@@ -12,23 +12,26 @@ const Card1: React.FunctionComponent<ICard1Props> = (props) => {
   const [onHover, setOnHover] = useState<boolean>(false);
   return (
     <div
-      className={`relative transition duration-500 h-[504px] w-1/3 rounded-xl shadow-lg flex flex-col text-center text-3xl ${
+      className={`relative transition duration-500 h-[210px] md:h-[504px] rounded-xl shadow-lg flex flex-col text-center text-3xl ${
         onHover ? "bg-color1 text-white" : ""
       }`}
       onMouseEnter={() => setOnHover(true)}
       onMouseLeave={() => setOnHover(false)}
     >
-      <div className="flex justify-center">
+      <div className="flex justify-center pt-4">
         <span
-          className={`flex justify-center my-10 rounded-full border-2 ${
+          className={`flex justify-center mb-4 md:my-10 rounded-full border-2 ${
             onHover ? "border-color2 text-color2" : "border-black text-black"
-          } h-[4rem] w-[4rem] items-center`}
+          } h-[40px] w-[40px] md:h-[4rem] md:w-[4rem] items-center text-base md:text-3xl`}
         >
           {props.children}
         </span>
       </div>
-      <h1 className="font-bold mx-8">{props.title}</h1>
-      <div className="absolute bottom-16 text-center w-full">
+      <h1 className="font-bold md:mx-8 text-xs mb-4 md:my-0 md:text-3xl tracking-tight md:tracking-normal">
+        {props.title}
+      </h1>
+
+      <div className="md:absolute md:bottom-16 static text-center w-full text-xs md:text-3xl tracking-tight md:tracking-normal ">
         <h1 className="mx-auto">{props.number}</h1>
         <h1 className="mx-auto">{props.date}</h1>
       </div>
--- src/components/LokasiStrategisComponent1.tsx
+++ src/components/LokasiStrategisComponent1.tsx
@@ -8,8 +8,9 @@ const LokasiStrategisComponent1: React.FunctionComponent<
   ILokasiStrategisComponent1Props
 > = (props) => {
   return (
-    <section className="mb-[10rem] mt-[7rem]">
-      <div className="flex justify-end">
+    <section className="mb-[5rem] md:mb-[10rem] mt-[2rem] md:mt-[7rem]">
+      {/* desktop version */}
+      <div className="hidden justify-end md:flex">
         <div id="itskampus">
           <Image
             width="900"
@@ -32,6 +33,37 @@ const LokasiStrategisComponent1: React.FunctionComponent<
             </div>
           </div>
           <div className="border-color1 border-b-8 w-[850px] h-[336px] text-2xl pl-[50px] py-[50px] pr-[150px]">
+            Aksesibilitas yang mudah berlokasikan dekat dengan kampus ITS
+            membuat perjalanan menjadi lebih cepat dan efisien bagi mahasiswa,
+            dosen, dan tenaga pendidik lainnya.
+          </div>
+        </div>
+      </div>
+
+      {/* mobile version */}
+      <div className="flex w-[390px] justify-end md:hidden">
+        <div id="itskampus">
+          <Image
+            width="182"
+            height="326"
+            src="/its2.webp"
+            alt="its"
+            className="object-cover"
+          ></Image>
+        </div>
+        <div className="flex flex-col">
+          <div className="h-[140px] w-[187px] bg-color1 p-3">
+            <h1 className="text-color2 text-xl">
+              <b>5 min</b> to
+            </h1>
+            <div className="text-xs font-semibold text-white mt-2">
+              <h1 className="tracking-widest">
+                ITS campus <br />
+                (Institut Teknologi Sepuluh Nopember)
+              </h1>
+            </div>
+          </div>
+          <div className="border-color1 border-b-2 text-xs w-[187px] h-[186px] px-5 pt-6 text-left tracking-tight">
             Aksesibilitas yang mudah berlokasikan dekat dengan kampus ITS
             membuat perjalanan menjadi lebih cepat dan efisien bagi mahasiswa,
             dosen, dan tenaga pendidik lainnya.
