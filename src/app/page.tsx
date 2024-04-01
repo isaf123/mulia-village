4378f75aa4b4d1956f5313ed94e539c5fa4548e5
isaf123 <f.isa283@gmail.com>
2024-04-01 13:36:54 +0700

    update-vercell

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,14 @@
-
 import Footer from "@/view/Footer";
 import ManagedBy from "@/view/ManagedBy";
-import Image from "next/image";
 import Faq from "@/view/FAQ";
 import Fasilitas from "@/view/Fasilitas";
 import LokasiStrategis from "@/view/lokasi_strategis";
-import Image from "next/image";
 import Divider1 from "@/components/Divider1";
-import Divider2 from "@/components/DIvider2";
 import HeroSection from "@/view/HeroSection";
 import AboutUs from "@/view/AboutUs";
 import LegalitasTerjamin from "@/view/legalitas_terjamin";
 import TypeRumah from "@/view/typerumah";
 import TestimonialsCustomer from "@/view/testimonials_customer";
-
-
-
-
 
 export default function Home() {
   return (
@@ -30,12 +22,10 @@ export default function Home() {
       <LegalitasTerjamin />
       <Divider1>Testimonials from</Divider1>
       <TestimonialsCustomer />
-      
+
       <Faq />
       <ManagedBy />
       <Footer />
-
     </div>
-
   );
 }
