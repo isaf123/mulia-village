64f8cc055bb0e3c6f5dccc79f7100990ce631817
isaf123 <f.isa283@gmail.com>
2024-04-16 14:52:53 +0700

    update code pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
               });
           `}
         </Script>
-        <Script id="facebook-pixel">
+        {/* <Script id="facebook-pixel">
           {`
             !function(f,b,e,v,n,t,s)
             {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
@@ -46,6 +46,20 @@ export default function RootLayout({
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
             fbq('init', '457090476743382');
+            fbq('track', 'PageView');
+          `}
+        </Script> */}
+        <Script id="facebook-pixel">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '456616000363128');
             fbq('track', 'PageView');
           `}
         </Script>
